129b17cf1298ed4887eb479ed4610950c4963490
Weston Sandfort <sandfortw@gmail.com>
2023-06-05 13:24:17 -0600

    run rubocop

--- spec/facades/magic8_facade_spec.rb
+++ spec/facades/magic8_facade_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Magic8Facade, type: :facade do
   describe '#generate_response' do
     it 'should generate a response when a string is provided' do
       response = Magic8Facade.new.generate_response('Will I become rich and famous?')
-      p "Good response:"
+      p 'Good response:'
       p response
       expect(response).to be_a String
     end
