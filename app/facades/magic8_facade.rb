97ca0c7412aa450870f5bf0915c01fa291cae960
Weston Sandfort <sandfortw@gmail.com>
2023-06-05 10:56:26 -0600

    build out controller and run rubocop

--- app/facades/magic8_facade.rb
+++ app/facades/magic8_facade.rb
@@ -3,5 +3,7 @@
 class Magic8Facade
   def generate_response(info)
     Magic8Service.new.generate_response(info.question)
+  rescue StandardError
+    'Jeff the great did not like that, try again!'
   end
 end
